07117cf05121eafa866f197377f9ac0035b81da3
apcabotaje <apcabotaje@gmail.com>
2024-08-02 19:54:57 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+const body = document.body
+
+const div = document.querySelector('div')
+const spanHi = document.querySelector('#hi')
+const spanBye = document.querySelector('#bye')
+
+spanHi.classList.add("new-class")
+spanHi.classList.remove("new-class")
+spanHi.classList.toggle("new-class")
+spanHi.classList.toggle("hi1")
+
+spanBye.style.color = "red"
+spanBye.style.backgroundColor = "green"
